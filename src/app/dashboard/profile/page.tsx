2adf44ac724026aa19393c7e2c5350402bbeacdf
maximoortelli <maximoortelli@hotmail.com>
2024-04-09 11:20:49 -0300

    Addded all files
    Addded all files

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,14 +1,14 @@
 "use client";
-import {useSession} from "next-auth/react";
+import {useSession, signOut} from "next-auth/react";
 
 function ProfilePage(){
 
     const {data: session, status} = useSession();
     console.log(session, status);
   return (
-    <div>
-       <h1>Profile</h1>
-       <pre>
+    <div className="justify-center flex flex-col items-center gap-y-5">
+       <h1 className="font-bold text-3xl">Profile</h1>
+       <pre className="bg-zinc-800 p-4">
          {JSON.stringify(
           {
             session,
@@ -16,6 +16,9 @@ function ProfilePage(){
           },
          )}
        </pre>
+       <button className="bg-zinc-800 px-4 py-2 block mb-2" onClick={() => {signOut()}}>
+         Logout
+       </button>
     </div>
   )
 }

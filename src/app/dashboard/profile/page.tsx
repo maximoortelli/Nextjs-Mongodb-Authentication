d0112c53b76799aaa85ac088f3b6a3ff65ac5683
maximoortelli <maximoortelli@hotmail.com>
2024-04-08 23:21:52 -0300

    Added all files
    Added all files

---
+++ src/app/dashboard/profile/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import {useSession} from "next-auth/react";
+
+function ProfilePage(){
+
+    const {data: session, status} = useSession();
+    console.log(session, status);
+  return (
+    <div>
+       <h1>Profile</h1>
+       <pre>
+         {JSON.stringify(
+          {
+            session,
+            status,
+          },
+         )}
+       </pre>
+    </div>
+  )
+}
+
+export default ProfilePage;
